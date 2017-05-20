2dca09496e023195c039d2cdcb167c7be8e4849f
John Hankinson <john.hankinson@landmark.co.uk>
2017-05-20 19:18:57 +0100

    name change

--- hjwasm64.cmd
+++ hjwasm64.cmd
@@ -5,7 +5,7 @@
 echo @SET SRC=..\HJWasm-master
 cl cl -c -Zl -Zp8 /GS- /Ox /TC /W2 /Gm- /Zc:inline /fp:precise /WX- /Zc:forScope /Gd /Ot /MT -I H *.c
 
-cl main.obj apiemu.obj orgfixup.obj assemble.obj assume.obj atofloat.obj backptch.obj bin.obj branch.obj cmdline.obj codegen.obj coff.obj condasm.obj context.obj cpumodel.obj data.obj dbgcv.obj directiv.obj elf.obj end.obj equate.obj errmsg.obj expans.obj expreval.obj extern.obj fastpass.obj fixup.obj fpfixup.obj hll.obj input.obj invoke.obj label.obj linnum.obj listing.obj loop.obj lqueue.obj macro.obj macrolib.obj mangle.obj memalloc.obj msgtext.obj omf.obj omffixup.obj omfint.obj option.obj parser.obj posndir.obj preproc.obj proc.obj queue.obj reswords.obj safeseh.obj segment.obj simsegm.obj string.obj symbols.obj tbyte.obj tokenize.obj trmem.obj types.obj libc.lib bufferoverflowu.lib -Fehasm64.exe
+cl main.obj apiemu.obj orgfixup.obj assemble.obj assume.obj atofloat.obj backptch.obj bin.obj branch.obj cmdline.obj codegen.obj coff.obj condasm.obj context.obj cpumodel.obj data.obj dbgcv.obj directiv.obj elf.obj end.obj equate.obj errmsg.obj expans.obj expreval.obj extern.obj fastpass.obj fixup.obj fpfixup.obj hll.obj input.obj invoke.obj label.obj linnum.obj listing.obj loop.obj lqueue.obj macro.obj macrolib.obj mangle.obj memalloc.obj msgtext.obj omf.obj omffixup.obj omfint.obj option.obj parser.obj posndir.obj preproc.obj proc.obj queue.obj reswords.obj safeseh.obj segment.obj simsegm.obj string.obj symbols.obj tbyte.obj tokenize.obj trmem.obj types.obj libc.lib bufferoverflowu.lib -Feuasm64.exe
 @DEL *.obj
 pause
 
