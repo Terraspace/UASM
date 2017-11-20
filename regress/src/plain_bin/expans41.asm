d204a9ed646aaedcb868a319d2d553cfcdea4253
unknown <john@terraspace.co.uk>
2017-11-20 15:09:06 +0000

    update regression tests

---
+++ regress/src/plain_bin/expans41.asm
@@ -0,0 +1,33 @@
+
+;--- macro function calls with <>-literals as arguments of hll directives
+;--- this was a problem before v2.11 because hll directives .if, .while, .until, .elseif
+;--- trigger a special tokenize-mode that handles < and > differently
+
+	.386
+	.model flat
+	.code
+
+ifdef __ASMC__
+	option asmc:on
+endif
+	.if @SubStr(<ebx==1 && edi==2, xxx>, 1, 16)
+	.elseif @SubStr(<ebx==2 && edi==1, xxx>, 1, 16)
+	.endif
+
+ifdef __ASMC__
+	option asmc:off
+endif
+
+	.if @SubStr(<ebx==1 && edi==2, xxx>, 1, 16)
+	.elseif @SubStr(<ebx==2 && edi==1, xxx>, 1, 16)
+	.endif
+
+ifdef __ASMC__
+	option asmc:on
+endif
+
+;--- this was accepted prior to v2.11, but result of @InStr() was wrong
+	.if @InStr(,<abc>,<c>)
+	.endif
+
+end
