0a0491bb2f48c6aa5332e76f1f5d8869ce5021a3
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-10-18 14:53:57 +0200

    char const *srcName;
    macho64.c:289:16: warning: assignment discards ‘const’ qualifier from pointer target type [-Wdiscarded-qualifiers]
      pSec->srcName = srcName;

--- macho64.c
+++ macho64.c
@@ -42,7 +42,7 @@ struct strentry
 
 struct macho_section_entry
 {
-	char *srcName;
+	char const *srcName;
 	struct section_64 section;
 	struct section_64 *next;
 	int dif; /* padding to keep section size multiple of 16 */
@@ -280,7 +280,7 @@ static int GetSymbolIndex(const char *pName, struct macho_module *mm)
 /* ==========================================================================================
 Build a macho_section_entry structure.
 ========================================================================================== */
-struct section_64 * macho_build_section( const char *secName, const char *segName, uint32_t flags, const char *srcName )
+struct section_64 * macho_build_section( const char secName[], const char segName[], uint32_t flags, const char srcName[] )
 {
 	struct macho_section_entry *pSec = NULL;
 	pSec = malloc(sizeof(struct macho_section_entry));
